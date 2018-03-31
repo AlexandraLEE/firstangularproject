a274e21148a55362dcf91e86e60031d290028e0a
Alexandra LEE <palexalee@gmail.com>
2018-03-31 18:55:09 +0200

    "adding findprecious"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
+// Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Libraries
 
+//Project files
 import { AppComponent } from './app.component';
 
+import { IntfinaleComponent } from './intfinale/intfinale.component';
 
+const appRoutes: Routes = [
+  { path: 'findprecious', component: IntfinaleComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    IntfinaleComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
